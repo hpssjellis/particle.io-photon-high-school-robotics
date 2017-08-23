4cf67e10349bd530dc9f075fee731c443caf47e0
Jeremy Ellis <keyfreemusic@gmail.com>
2017-08-22 22:03:20 -0700

    Update a01-console.ino

--- a01-console/a01-console.ino
+++ a01-console/a01-console.ino
@@ -2,32 +2,35 @@
 // By Jeremy Ellis
 
 // lots of sensors have these labels 
-// GND    Ground
-// VCC     Normal 5Vin check board may want 3V3
+// GND     Ground
+// VCC     Normal  Vin check the board may work with 3V3
 // NC      Not Connected - might be labelled LED 
-// Sig      Connect to A0 
+// Sig     Connect to A0 
 
-// You can run this program without anything connected to A0
 
 int   x = 0;
 float y = 0; 
 
+
 void setup() {
 
 }
+
+
+
 
 void loop() {
 
   x += 1;
   y = rand() / 10000000.0;    //Note: decimal makes float division. If integer result would be an integer!
   
-  Particle.publish("Integer x = ", String(x), 60, PRIVATE);  
+  Particle.publish("Integer x = ", "Count #" + String(x), 60, PRIVATE);  //shows printing an integer variable
   delay(1000); 
-  Particle.publish("Random decimal y = ", String(y,3), 60, PRIVATE);  // show a float to 3 decimal places
+  Particle.publish("Random Decimal y = ", String(y, 3), 60, PRIVATE);  // show a float to 3 decimal places
   delay(1000); 
-  Particle.publish("Analog Read A0 = ", String(analogRead(A0)), 60, PRIVATE);  
+  Particle.publish("Analog Read A0 = ", String(analogRead(A0)), 60, PRIVATE);  // shows A0
   delay(1000); 
-  Particle.publish(" . "," . ", 60, PRIVATE);    // just to show a space
+  Particle.publish("----------------","-------------", 60, PRIVATE);    // just to show a space between samples
   delay(6000); // wait about 6 seconds
 
 }
