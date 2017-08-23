13a101f52510953ade035ec7d9736ce4687487d3
Jeremy Ellis <keyfreemusic@gmail.com>
2017-08-22 22:30:41 -0700

    Update a02-voltage-divider.ino

--- a02-voltage-divider/a02-voltage-divider.ino
+++ a02-voltage-divider/a02-voltage-divider.ino
@@ -1,7 +1,7 @@
 // Voltage Divider
 // By Jeremy Ellis
 
-// GND to resistor 
+// GND to resistor (try different resistors i used a 5 k ohm, larger resistor larger range of values)
 // resistor to both A0 and a 2 prong sensor (such as a photoresistor)
 // other side of 2 prong sensor to 3V3
 
@@ -15,5 +15,4 @@ void loop() {
   delay(6000); // wait about 6 seconds
 
 }
-
 
