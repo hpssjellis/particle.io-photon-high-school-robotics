dc3ce85d0c35330f9941cf7d1a46457295b526b1
Jeremy Ellis <keyfreemusic@gmail.com>
2017-08-24 23:45:25 -0700

    Update big-check.ino

--- a11-dc-motor-drivers/big-check.ino
+++ a11-dc-motor-drivers/big-check.ino
@@ -11,6 +11,7 @@ void setup() {
     pinMode(A5, OUTPUT);   // PWM 0 to 255
     pinMode(D5, OUTPUT);   // digital 0 to 1
     pinMode(D6, OUTPUT);   // digital 0 to 1
+    
     pinMode(D7, OUTPUT);
 
 }
@@ -20,16 +21,17 @@ void loop() {
     digitalWrite(D7, 1);    // D7 on
     
     digitalWrite(D5, 1);    // set one direction
-    digitalWrite(D6, 0);    
+    digitalWrite(D6, 0);    // set one direction 
+    
     analogWrite(A5, 50);   // go medium
     delay(3000);
     
-    analogWrite(A4, 0);     // stop
+    analogWrite(A5, 0);     // stop
     delay(3000); 
    
 
     digitalWrite(D5, 0);    // set other direction
-    digitalWrite(D6, 1);    
+    digitalWrite(D6, 1);    // set one direction
     
     analogWrite(A5, 50);   // go medium   
     delay(1000);    
@@ -38,7 +40,6 @@ void loop() {
     delay(1000);
     
     analogWrite(A5, 0);     // stop
-    
     digitalWrite(D7, 0);    // D7 off
     delay(9000);            // wait 9 seconds
         
