eb424f7d3e77cd6ead0ecbf176cc68cb865b13ab
Jeremy Ellis <keyfreemusic@gmail.com>
2017-08-25 21:20:37 -0700

    Update a14-serial-I2C.ino

--- a14-serial-I2C/a14-serial-I2C.ino
+++ a14-serial-I2C/a14-serial-I2C.ino
@@ -1,16 +1,19 @@
+
+// This #include statement was automatically added by the Particle IDE.
+#include "Adafruit_SSD1306.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "Adafruit_GFX.h"
+
 /*********************************************************************
 This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
   Pick one up today in the adafruit shop!
   ------> http://www.adafruit.com/category/63_98
-
 This example is for a 128x64 size display using I2C to communicate
 3 pins are required to interface (2 I2C and one reset)
-
 Adafruit invests time and resources providing this open source code, 
 please support Adafruit and open-source hardware by purchasing 
 products from Adafruit!
-
 Written by Limor Fried/Ladyada  for Adafruit Industries.  
 BSD license, check license.txt for more information
 All text above, and the splash screen must be included in any redistribution
@@ -116,9 +119,7 @@ void setup()   {
   delay(2000);
  
  
-  display.clearDisplay();
-  display.display();
+ // display.clearDisplay();
+ // display.display();
   
 }
-
-
