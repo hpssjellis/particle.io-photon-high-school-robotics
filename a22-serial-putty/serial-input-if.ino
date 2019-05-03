eebd4ed0d21655610bb1d24dfe5f9e0b256fb388
Jeremy Ellis <keyfreemusic@gmail.com>
2019-05-03 09:26:58 -0700

    Create serial-input-if.ino

---
+++ a22-serial-putty/serial-input-if.ino
@@ -0,0 +1,29 @@
+
+int incomingByte = 0; // for incoming serial data
+
+void setup() {
+    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+    Serial.print("Press any key, then try the letter A");
+}
+
+void loop() {
+  // send data only when you receive data:
+    if (Serial.available() > 0) {
+    // read the incoming byte:
+        incomingByte = Serial.read();
+
+        Serial.print("I received: ");
+        Serial.println(incomingByte, DEC);
+        
+        if ((char)incomingByte == 'A'){   // #65
+            Serial.println("Cool, you entered an 'A' ");
+ 
+        }        
+        if ((char)incomingByte == 'a'){   // #97
+            Serial.println("Sweet, you entered an 'a' ");
+ 
+        }
+        
+    }
+  
+}
