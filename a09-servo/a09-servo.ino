71ee8e5a1042ef7e8458cbfff16ef69af5927bd7
Jeremy Ellis <keyfreemusic@gmail.com>
2019-05-08 14:40:41 -0700

    Update a09-servo.ino

--- a09-servo/a09-servo.ino
+++ a09-servo/a09-servo.ino
@@ -2,6 +2,8 @@
 // By Jeremy Ellis
 // MIT License
 
+
+// MUST HAVE A 6 VOLT EXTERNAL BATTERY
 // servo red to 6V battery positive
 // servo ground (brown or black) to 6V battery negative
 // servo ground to Photon GND
@@ -11,34 +13,45 @@
 // D0, D1, A4, A5, WKP, RX, TX 
 // D0, D1, D2, D3, WKP, RX, TX
 
-Servo myServo;
+Servo myServo4;
 
 void setup() {
-    myServo.attach(A4);
+   // myServo.attach(A4);
     pinMode(D7, OUTPUT);
 
 }
 
 void loop() {
+    
+    myServo4.attach(A4);
+    
+    
     digitalWrite(D7, 1);       // D7 on    
     
-    myServo.write(0);          // servo at 0 degrees minimum
+    myServo4.write(0);          // servo at 0 degrees minimum
     delay(2000);
     
-    myServo.write(180);        // servo at 180 degrees Maximum
+    myServo4.write(180);        // servo at 180 degrees Maximum
     delay(2000);
 
-    myServo.write(90);         // servo at 90 degrees
+    myServo4.write(90);         // servo at 90 degrees
     delay(2000);   
     
     
     for (int myLoop = 0; myLoop <= 180; myLoop++){
-        myServo.write(myLoop);
+        myServo4.write(myLoop);
         delay(20);
     }
 
 
     digitalWrite(D7, 0);     // D7 off
     delay(5000);            // 5 second delay
+    
+    
+    myServo4.detach();   // less weird stuff if you detach the servo
 
 }
+
+
+
+
