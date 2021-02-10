cb23649cdd187a8eb7ceedf0f43ee9f398acf2fd
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-10 10:57:20 -0800

    Update a24-I2C-nano-sender.ino

--- a24i2c/a24-I2C-nano-sender.ino
+++ a24i2c/a24-I2C-nano-sender.ino
@@ -2,16 +2,16 @@ uint8_t val = 0;
 
 void setup() {
     Wire.begin();
-    pinMode(LEDB,OUTPUT); // the blue LED
+    pinMode(LEDB, OUTPUT); // the blue LED
 }
 
 void loop() {
     delay(1000);
-    digitalWrite(LEDB,HIGH);
+    digitalWrite(LEDB, 0);    // LOW turns this on on
     Wire.beginTransmission(8);
-    Wire.write(&val,1);
+    Wire.write(&val, 1);
     Wire.endTransmission();
     val++;
     delay(20);
-    digitalWrite(LEDB,LOW);
+    digitalWrite(LEDB, 1);  // 1 or HIGH turns this LED off
 }
